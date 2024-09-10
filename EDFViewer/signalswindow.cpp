afe397e530a51bbce4da2b2d58ba0090f38487bb
Kostas <kostasrotas@yahoo.com>
2024-09-10 10:10:49 +0300

    correct appearance in signals

--- EDFViewer/signalswindow.cpp
+++ EDFViewer/signalswindow.cpp
@@ -29,7 +29,7 @@ QWidget *signalwp;
 
 void closesignalswindow(){
     int i;
-    for (i=0;i<edfh.numofsignals;i++)
+    for (i=0;i<edfh.numofgraphsingals();i++)
         showsignal[i]=signalcb[i]->isChecked()?1:0;
     signalwp->close();
     paintWidget->repaint();
@@ -38,12 +38,12 @@ void closesignalswindow(){
 void seldeselall(){
     int i;
     Qt::CheckState newvalue=Qt::Checked;
-    for (i=0;i<edfh.numofsignals;i++)
+    for (i=0;i<edfh.numofgraphsingals();i++)
         if (signalcb[i]->isChecked()){
             newvalue=Qt::Unchecked;
             break;
         }
-    for (i=0;i<edfh.numofsignals;i++){
+    for (i=0;i<edfh.numofgraphsingals();i++){
         signalcb[i]->setCheckState(newvalue);
     }
 }
@@ -53,9 +53,9 @@ void signalswindow(){
     char str2[256];
     char useTips;
     float phmin,phmax;
-    int spr,digmin,digmax;\
-        char name[256],dim[256];
-    int i;
+    int spr,digmin,digmax;
+    char name[256],dim[256];
+    int i,j;
     int numOfColums=1;
     if (fileisopen==0)
         return;
@@ -63,43 +63,47 @@ void signalswindow(){
     QPushButton *button;
     QVBoxLayout *vb = new QVBoxLayout;
     QHBoxLayout *hb2,*hb1,*hb3;
+    QVBoxLayout *vbn[10];
     signalwp->setAttribute(Qt::WA_DeleteOnClose);
     signalwp->setWindowModality(Qt::ApplicationModal);
     signalwp->setWindowTitle("EDF Signals");
     signalwp->setLayout(vb);
-    if (edfh.numofsignals<=24){
+    if (edfh.numofgraphsingals()<=24){
         useTips=0;
-        numOfColums=1+edfh.numofsignals/8;
+        numOfColums=1+edfh.numofgraphsingals()/8;
     }
     else{
         useTips=1;
-        numOfColums=1+edfh.numofsignals/20;
+        numOfColums=1+edfh.numofgraphsingals()/16;
     }
-    for (i=0;i<edfh.numofsignals;i++){
-        if (i%numOfColums==0){
-            hb1=new QHBoxLayout;
-            vb->addLayout(hb1);
-        }
+    hb1=new QHBoxLayout;
+    vb->addLayout(hb1);
+    for (i=0;i<numOfColums;i++){
+        vbn[i]=new QVBoxLayout;
+        hb1->addLayout(vbn[i],1);
+    }
+    for (i=0;i<edfh.numofgraphsingals();i++){        
         hb2=new QHBoxLayout;
         signalcb[i]=new QCheckBox();
         signalcb[i]->setCheckState(showsignal[i]!=0?Qt::Checked:Qt::Unchecked);
         hb2->addWidget(signalcb[i]);
-        edfh.getsignaldata(i,name,NULL,dim,&phmin,&phmax,&digmin,&digmax,NULL,&spr);
+        edfh.getsignaldata(i,name,NULL,dim,&phmin,&phmax,&digmin,&digmax,NULL,&spr);        
+        sprintf(str,"Signal %d [%s]",i+1,name);
+        sprintf(str2,"Signal %d [%s]\nPhysical %.2f...%.2f%s\nDigital :%d...%d\nSamples per record :%d",i+1,name,phmin,phmax,dim,digmin,digmax,spr);
+        QLabel* label=new QLabel();
         if (useTips){
-            sprintf(str,"Signal %d [%s]",i+1,name);
-            sprintf(str2,"Physical %.2f...%.2f%s\nDigital :%d...%d\nSamples per record :%d\n",phmin,phmax,dim,digmin,digmax,spr);
+            label->setText(str);
+            label->setToolTip(str2);
         }
         else{
-            sprintf(str,"Signal %d [%s]\nPhysical %.2f...%.2f%s\nDigital :%d...%d\nSamples per record :%d\n",i+1,name,phmin,phmax,dim,digmin,digmax,spr);
+            label->setText(str2);
         }
-
-        QLabel* label=new QLabel(str);
         hb2->addWidget(label);
-        if (useTips)
-            label->setToolTip(str2);
         hb2->setStretch(hb2->count()-1,1);
-        hb1->addLayout(hb2);
+        vbn[i%numOfColums]->addLayout(hb2);
     }
+    for (j=0;j<(numOfColums-i%numOfColums)%numOfColums;j++)
+        vbn[numOfColums-1-j]->addWidget(new QLabel(""));
     hb3=new QHBoxLayout;
     button = new QPushButton("Deselect/Select All");
     QObject::connect(button,&QPushButton::clicked,seldeselall);
